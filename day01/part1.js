047e8fd5ebf08c1ec55b78d5ab7e0cbacd7e73f5
Javed Akhtar <akhtar.ja@gmail.com>
2021-12-02 01:39:00 -0800

    day 1

---
+++ day01/part1.js
@@ -0,0 +1,18 @@
+import { readFileSync } from 'fs';
+
+const increasesCount = () => {
+  let count = 0;
+  const filePath = 'day01/input.txt';
+  const inputs = readFileSync(filePath, { encoding: 'utf8' })
+    .split('\n')
+    .map((input) => parseInt(input));
+
+  inputs.forEach((input, index) => {
+    if (index !== 0 && input > inputs[index - 1]) {
+      count++;
+    }
+  });
+  return count;
+};
+
+console.log(`\nDay 1, part 1: ${increasesCount()}\n`);
---
+++ day01/part2.js
@@ -0,0 +1,24 @@
+import { readFileSync } from 'fs';
+import { parse } from 'path';
+
+const increasingWindowCount = () => {
+  let count = 0;
+  let curr;
+  let prev;
+  const filePath = 'day01/input.txt';
+  const inputs = readFileSync(filePath, { encoding: 'utf8' })
+    .split('\n')
+    .map((input) => parseInt(input));
+
+  inputs.forEach((input, index) => {
+    if (index >= 1 && index <= inputs.length - 3) {
+      curr = inputs[index] + inputs[index + 1] + inputs[index + 2];
+      prev = inputs[index] + inputs[index + 1] + inputs[index - 1];
+      if (curr > prev) count++;
+    }
+  });
+
+  return count;
+};
+
+console.log(`\nDay 1, part 2: ${increasingWindowCount()}\n`);
