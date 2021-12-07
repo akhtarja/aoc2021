4cb554951aa5c339fdd2e0e70a0068ecf3b6654d
Javed Akhtar <akhtar.ja@gmail.com>
2021-12-06 22:25:55 -0800

    day 4 part 2

--- day04/part2.js
+++ day04/part2.js
@@ -8,7 +8,7 @@ const finalScore = () => {
   let rowData;
   let currentNumber;
   let bingo = 0;
-  const filePath = 'day04/testInput.txt';
+  const filePath = 'day04/input.txt';
   const inputs = readFileSync(filePath, { encoding: 'utf8' }).split('\n');
   const numbersToDraw = inputs[0]
     .split(',')
@@ -33,19 +33,20 @@ const finalScore = () => {
     if (bingo === 0) {
       currentNumber = value;
       boards = playBingo(boards, value);
-      if (value == 24)
-        if (boards.length === 1)
-          bingo = completeColumn(boards) || completeRow(boards);
+      if (boards.length === 1) {
+        bingo = completeColumn(boards) || completeRow(boards);
+      }
     }
   });
 
-  return currentNumber;
+  return bingo * currentNumber;
 };
 
 const parseRow = (rowData, start, end) =>
   parseInt(rowData.substring(start, end), 10);
 
 const playBingo = (boards, value) => {
+  let mutatedBoards = [];
   for (let board = 0; board < boards.length; board++) {
     for (let boardRow = 0; boardRow < boards[board].length; boardRow++) {
       for (
@@ -58,13 +59,16 @@ const playBingo = (boards, value) => {
         }
       }
     }
-
-    if (completeColumn([boards[board]] || completeRow[boards[board]] > 0)) {
-      boards.splice(board, 1);
-      return boards;
-    }
   }
-  return boards;
+
+  boards.forEach((board) => {
+    if (completeColumn([board]) === 0 && completeRow([board]) === 0) {
+      mutatedBoards.push(board);
+    }
+  });
+
+  if (mutatedBoards.length === 0) return boards;
+  return mutatedBoards;
 };
 
 const completeColumn = (boards) =>
@@ -74,10 +78,8 @@ const completeRow = (boards) => {
   let unmarkedSum = 0;
   boards.forEach((board) => {
     board.forEach((boardRow) => {
-      if (boardRow.reduce((a, b) => a + b) === -5) {
-        console.log(boards);
+      if (boardRow.reduce((a, b) => a + b) === -5)
         unmarkedSum = sumOfUnmarkedItems(board);
-      }
     });
   });
   return unmarkedSum;
