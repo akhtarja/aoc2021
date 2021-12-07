926dc115936e44f4ec6adfaae0cd42e25c0c151f
Javed Akhtar <akhtar.ja@gmail.com>
2021-12-07 01:40:31 -0800

    day 7 part 1

--- day07/part1.js
+++ day07/part1.js
@@ -1,12 +1,24 @@
 const { readFileSync } = require('fs');
 
 const fuelConsumption = () => {
-  const filePath = 'day07/testInput.txt';
+  const filePath = 'day07/input.txt';
   const inputs = readFileSync(filePath, { encoding: 'utf8' })
     .split(',')
     .map((input) => parseInt(input, 10));
+  let lastFuel = Math.max(...inputs) * inputs.length;
+  let currentFuel;
 
-  return inputs;
+  for (
+    let position = Math.min(...inputs);
+    position <= Math.max(...inputs);
+    position++
+  ) {
+    currentFuel = 0;
+    inputs.forEach((input) => (currentFuel += Math.abs(input - position)));
+    if (currentFuel < lastFuel) lastFuel = currentFuel;
+  }
+
+  return lastFuel;
 };
 
 console.log(`\nDay 7, part 1: ${fuelConsumption()}\n`);
