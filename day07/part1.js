6ab14ae4f7f535872dcaf1db0669573d732d68d4
Javed Akhtar <akhtar.ja@gmail.com>
2021-12-07 01:21:27 -0800

    day 6 part 2

---
+++ day07/part1.js
@@ -0,0 +1,12 @@
+const { readFileSync, readFile } = require('fs');
+
+const fuelConsumption = () => {
+  const filePath = 'day07/testInput.txt';
+  const inputs = readFileSync(filePath, { encoding: 'utf8' })
+    .split(',')
+    .map((input) => parseInt(input, 10));
+
+  return inputs;
+};
+
+console.log(`\nDay 7, part 1: ${fuelConsumption()}\n`);
